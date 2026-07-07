81f1506889492fcb0b16ec1ff98eb329f76eb228
Alison <alison.lei06@gmail.com>
2026-07-07 02:49:40 -0400

    working on bus, Harvard architecture

--- rtl/top_processor.sv
+++ rtl/top_processor.sv
@@ -8,6 +8,10 @@ module top_processor (
     logic branch_b, jump_b;
     logic [31:0] branch_addr, jump_addr;
 
+    // bus
+    logic dataram_sel, gpio_sel, spi_sel, interrupt_sel;
+    logic [31:0] target_mem_indx;
+
     // fetch
     logic [31:0] current_pc;
     logic [31:0] instr;
@@ -35,12 +39,17 @@ module top_processor (
     logic [4:0] last_reg_num;
     logic [31:0] last_reg_data;
 
-    
+    // implement Harvard architecture as fetch and memory state access memory at different times for different reasons
     // synchronous on pos clock edge
     update_pc pc0 (.clk(clk), .reset(reset), .branch_b(branch_b), .jump_b(jump_b), .branch_addr(branch_addr), .jump_addr(jump_addr), .out_pc(current_pc));
     
+    bus b0 (.reset(reset), .addr(mem_addr), .dataram_sel(dataram_sel), .gpio_sel(gpio_sel), .spi_sel(spi_sel),
+            .interrupt_sel(interrupt_sel), .target_mem_indx(target_mem_indx));
+
+    // can write current_pc as it will never go past the instruction partition in memory/surpass 32'h00000FFF
     fetch f0 (.addr(current_pc), .instr(instr));
-    
+
+    // havent changed from current_pc to target_mem_index
     decode d0 (.instr(instr), .ALU_ctrl(ALU_ctrl), .rd_num(rd_num), .rs1_num(rs1_num), .rs2_num(rs2_num), .immediate(immediate), 
                 .jump_enable(jump_enable), .branch_enable(branch_enable), .load_enable(load_enable), .store_enable(store_enable),
                 .rd_enable(rd_enable), .ALUSrc(ALUSrc), .use_pc(use_pc), .size(size), .branch_type(branch_type));
@@ -58,7 +67,7 @@ module top_processor (
 
     // store data written to memory on next clock edge
     memory_stage memstge0 (.clk(clk), .ld_enable(load_enable), .st_enable(store_enable), .size(size), .final_reg_num(rd_num),
-                            .st_data(rs2_data), .mem_addr(mem_addr), .load_reg_data(mem_stage_rd_data), .load_reg_num(mem_stage_rd_num));
+                            .st_data(rs2_data), .mem_addr(target_mem_indx), .load_reg_data(mem_stage_rd_data), .load_reg_num(mem_stage_rd_num));
 
     writeback wb0 (.ld_enable(load_enable), .rd_enable(rd_enable), .ex_final_reg_num(execute_rd_num), .mem_load_reg_num(mem_stage_rd_num),
                     .ex_final_reg_data(execute_rd_data), .mem_load_reg_data(mem_stage_rd_data), .last_reg_num(last_reg_num),
